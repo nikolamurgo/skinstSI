cf5eee8f40c210c2e95f70d6abec645e718a69c3
nikolamurgo <murgovskinikola@gmail.com>
2025-03-19 17:39:11 +0100

    express init and DBconn with basics

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 6060
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+//mongodb connection ----------------------------------------
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@skinst.dmmwk.mongodb.net/?retryWrites=true&w=majority&appName=skinst`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create a database and collections
+    const database = client.db("skinst")
+    app.use("/classes", require("./routes/classes")(database))
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// ---------------------------------------
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+

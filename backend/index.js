845f6c5ee46c5ca3b7d4cd6f8de9450a3737d397
nikolamurgo <murgovskinikola@gmail.com>
2025-03-19 18:35:49 +0100

    rollback

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,89 @@ async function run() {
 
     // create a database and collections
     const database = client.db("skinst")
-    app.use("/classes", require("./routes/classes")(database))
-    
+    const userCollection = database.collection("users")
+    const classesCollection = database.collection("classes")
+    const cartCollection = database.collection("cart")
+    const paymentCollection = database.collection("payments")
+    const enrolledCollection = database.collection("enrolled")
+    const appliedCollection = database.collection("applied")
 
+    // classes routes
+    app.post('/new-class', async (req, res) =>{
+        const newClass = req.body;
+        const result = await classesCollection.insertOne(newClass);
+        res.send(result);
+    })
+
+    app.get('/classes', async (req,res) =>{
+        const query = {status: 'approved'}
+        const result = await classesCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/classes/:email', async (req,res) =>{
+        const email = req.params.email
+        const query = {instructorEmail: email}
+        const result = await classesCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    // manage classes
+    app.get('/classes-manage', async (req,res) =>{
+      const result = await classesCollection.find().toArray()
+      res.send(result)
+    })
+
+    // update classes status and reason
+    app.patch('/change-status/:id', async (req,res) =>{
+      const id = req.params.id
+      const status = req.body.status
+      const reason = req.body.reason
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updateDoc = {
+        $set: {
+          status: status,
+          reason: reason,
+        },
+      }
+      const result = await classesCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })
+
+    // get apprived class
+    app.get('/approved-classes', async (req,res)=>{
+      const query = {status: 'approved'}
+      const result = await classesCollection.find(query).toArray()
+      res.send(result)
+    })
+    //get single class details
+    app.get('/classes/:id', async (req,res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await classesCollection.findOne(query)
+      res.send(result)
+    })
+
+    // update class details
+    app.put('/update-class/:id', async (req,res) =>{
+      const id = req.params.id
+      const updateClass = req.body
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+      const updateDoc = {
+        $set: {
+          name: updateClass.name,
+          description: updateClass.description,
+          price: updateClass.price,
+          availableSeats: parseInt(updateClass.availableSeats),
+          videolink: updateClass.videolink,
+          status: "pending",
+        }
+      }
+      const result = await classesCollection.updateOne(filter,updateDoc,options)
+      res.send(result)
+    })    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
